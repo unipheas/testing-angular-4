20016acb2474eb035cfd5eba0eab6c4f490170a1
Brian Phillips <brian.phillips@qq.com>
2017-09-03 18:15:43 +0800

    Setup and Tear Down

--- src/app/03-setup-and-teardown/vote.component.spec.ts
+++ src/app/03-setup-and-teardown/vote.component.spec.ts
@@ -1,9 +1,27 @@
+import { Component } from '@angular/core';
 import { VoteComponent } from './vote.component'; 
 
 describe('VoteComponent', () => {
-  it('', () => {
+  // Arrange
+  let component: VoteComponent;
+
+  beforeEach(() => {
+    component = new VoteComponent();
   });
 
-  it('', () => {
+  it('should increment totalVotes when upvoted', () => {
+    // Act
+    component.upVote();
+
+    // Assert
+    expect(component.totalVotes).toBe(1);
+  });
+
+  it('decrement totalVotes when downvotes', () => {
+    // Act
+    component.downVote();
+
+    // Assert
+    expect(component.totalVotes).toBe(-1);
   });
 });
