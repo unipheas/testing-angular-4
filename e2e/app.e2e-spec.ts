3d21fd1e0c19e0f54cb3eaf61151be12496eab6e
Brian Phillips <brian.phillips@qq.com>
2017-09-03 17:49:02 +0800

    Unit Testing Fundamentals

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { TestingDemoPage } from './app.po';
 
-describe('testing-demo App', function() {
+describe('testing-demo App', () => {
   let page: TestingDemoPage;
 
   beforeEach(() => {
     page = new TestingDemoPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
